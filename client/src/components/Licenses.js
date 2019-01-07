99964ce21fde03a491bdd2442eecf6c11a0c0695
JustinCata <jcata018@fiu.edu>
2019-01-07 18:57:22 -0500

    contact page

--- client/src/components/Licenses.js
+++ client/src/components/Licenses.js
@@ -3,6 +3,7 @@ import Connect from './assets/How/connect.jpeg';
 import Sell from './assets/How/sell.jpeg';
 import Share from './assets/How/share.jpeg';
 import Earn from './assets/How/earn.jpeg';
+import Icon1 from './assets/InsuredIcon.png';
 import './css/License.css';
 
 
@@ -11,8 +12,13 @@ const Licenses = ()=>{
     <div id='Licenses'>
       <div className='col-xs-12 Licenses'>
         <div className='col-xs-12 col-sm-4'>
-          <h4>Insured</h4>
-          <p>lorem ipsum</p>
+          <div className='col-xs-12 col-sm-6'>
+          <img src={Icon1} alt="License" />  
+          </div>
+          <div className='col-xs-12 col-sm-6'>
+            <h4>Insured</h4>
+            <p>lorem ipsum</p>
+          </div>      
         </div>
         <div className='col-xs-12 col-sm-4'>
           <h4>Experienced</h4>
