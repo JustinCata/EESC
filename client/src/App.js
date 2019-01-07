fb119d9ed7352d7f4e5cf72b30d2c831ec2edd70
abel30567 <claudio@imarkett.com>
2019-01-07 16:08:00 -0500

    delete old css and create services section

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import Navbar from './components/Navbar';
 import BannerDemo from './components/BannerDemo';
-import Why from './components/Why';
-import How from './components/How';
-import What from './components/What';
-import Who from './components/Who';
-import FAQ from './components/FAQ.js';
-import Subscribe from './components/Subscribe';
+import Services from './components/Services';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -18,121 +13,16 @@ class App extends Component {
     this.state = {
       count: ''
     };
-    this.getCount = this.getCount.bind(this);
   }
-
-
-// addyBalance() {
-//   tangle.api.getBalances([address], 10, (err, data) => {
-//     if (err){
-//       return
-//     };
-//     const bal = data.balances[0];
-//     const funds = (bal/1000000);
-//     this.setState({
-//       fundraised: funds
-//     });
-//   });
-// }
-
-// confirmTx() {
-//   tangle.api.findTransactionObjects({'addresses': [address]}, (err, data) => {
-
-//     if (err) {
-//       return
-//     };
-
-//     var txHash = [];
-//     var txState = [];
-//     var listContributions = [];
-
-//     data.forEach((transaction)=> {
-//       // console.log(transaction.hash); this gives txHashes
-//       txHash.push(transaction.hash);
-//     });
-
-//     tangle.api.getLatestInclusion(txHash, (e, states) => {
-
-//       if (e) {
-//         return
-//       }
-//       else {
-
-//         states.forEach((state) => {
-
-//           if (state == true){
-//             txState.push(state);
-//           }
-//         });
-
-//         this.setState({
-//           backers: txState.length
-//         });
-//       }
-//     });
-//   });
-// }
-
-
-// priceRender() {
-//     axios.get('https://api.coinmarketcap.com/v1/ticker/iota/')
-//       .then(res => {
-//         const IOTA = res.data;
-//         const IOTAprice = IOTA[0].price_usd;
-//         this.setState({
-//           price: IOTAprice
-//         });
-
-//        }).catch(error => {
-//          console.log("error:" + error);
-//        });
-// }
-
-componentDidMount() {
-  this.getCount();
-}
-
-// componentWillMount(){
-//   this.setState({
-//     price: 0
-//   });
-//   this.setState({
-//     backers: 0
-//   });
-//   this.setState({
-//     fundraised: 0
-//   });
-//   this.priceRender();
-//   this.addyBalance();
-//   this.confirmTx();
-// }
-
-  getCount() {
-    axios.get('https://metamesh.io/stats')
-              .then( (response) => {
-                // console.log('subscribers', response.data.subscribers);
-                this.setState({count: response.data.subscribers}, () => {
-                  console.log(this.state);
-                })
-              })
-              .catch((error) => {
-                console.log(error);
-              });
+  componentDidMount() {
+    
   }
-
-
   render() {
     return (
       <div className="App">
         <Navbar />
-        {/*<NewNav />*/}
         <BannerDemo />
-        {/* <Why count={this.state.count} /> */}
-        {/* <How /> */}
-        {/* <What /> */}
-        {/* <Who /> */}
-        {/* <FAQ /> */}
-        {/* <Subscribe /> */}
+        <Services />
         <Footer />
       </div>
     );
