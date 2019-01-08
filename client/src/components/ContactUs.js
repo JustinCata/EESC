269fdc66e3a89b641fdc4593164bc2da27d8886b
JustinCata <jcata018@fiu.edu>
2019-01-08 17:15:11 -0500

    made contact responsive

--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import phone from './assets/Design (39).png';
+import phone2 from './assets/phone.png';
 import mail from './assets/Design (38).png';
 import Footer from './Footer.js';
 import './css/contact.css';
@@ -49,25 +50,30 @@ class Contact extends Component {
           </div>
 
           <div className= "col-md-12 visit"> 
-            Visit Us
+            <p className='title'>Visit Us</p>
             <p>9380 Sw 72nd Street Suite B140,  </p>
             <p>Miami, FL 33173 </p>
           </div>
+
+          <a id="phone" href="tel:7864888657">
+          <img  src={phone2} />
+          </a>
+
           <div className= "col-md-12 call-mail">
-            <div className= "col-md-6">
-              <div className= 'col-md-2'>
+            <div className= "col-sm-6">
+              <div className= 'col-sm-3'>
                 <img src= {phone}/>
               </div>
-              <div className= 'col-md-10'>
+              <div className= 'col-sm-9'>
                 <p>Call us</p>
                 <h4>305.885.5255 | 786.488.8657</h4>
               </div>
             </div>
-            <div className= "col-md-6">
-              <div className= 'col-md-2 '>
+            <div className= "col-sm-6">
+              <div className= 'col-sm-3 '>
                 <img src= {mail}/>
               </div>
-              <div className= 'col-md-10'>
+              <div className= 'col-sm-9'>
               <p>Have any questions? Se habla Español</p>
                 <h4>Empireengineering@hotmail.com</h4>
               </div>
